ded9df329fddca904098da11375b27e795dd11e7
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-02-17 16:28:05 +1000

    Update main.c
    create

--- laba_04/main.c
+++ laba_04/main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include "myLib.h"
 
+int main()
+{
+    double s1, s2, s3;
+    printf("vvedite 1 storonu:");
+    scanf("%lf", &s1);
+    printf("vvedite 2 storonu:");
+    scanf("%lf", &s2);
+    printf("vvedite 3 storonu:");
+    scanf("%lf", &s3);
+    if (s1+s2>s3 && s2+s3>s1 && s3+s1>s2){
+        printf("perimeter: %lf, area: %lf", perimeter(s1, s2, s3), area(s1, s2, s3));
+    } else printf("invalid vvod");
+    return 0;
+}
