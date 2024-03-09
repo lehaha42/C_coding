427ef31baeff53b6b9239d60633c97965a6482a6
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-03-09 12:35:47 +1000

    Update func.c

--- laba_05/func.c
+++ laba_05/func.c
@@ -8,11 +8,32 @@ double** calc(int n, double **arr1, double **arr2, char c){
     arr = (double**)malloc(n*sizeof(double*));
     for (int i = 0; i<n; i++) arr[i] = (double*)malloc(n*sizeof(double));
     
-    for (int i = 0; i<n; i++){
-        for (int j = 0; j<n; j++){
-            arr[i][j] = arr1[i][j] + arr2[i][j];
+    if (c == '+'){
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<n; j++){
+                arr[i][j] = arr1[i][j] + arr2[i][j];
+            }
         }
-    }
+    }    
+    
+    if (c == '-'){
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<n; j++){
+                arr[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+    }    
+    
+    if (c == '*'){
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<n; j++){
+                arr[i][j] = 0;
+                for (int k = 0; k<n; k++){
+                    arr[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+    }    
     
     return arr;
 }
