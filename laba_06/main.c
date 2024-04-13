596b3d5fab3ad42e4a710fc4cf4893b74a9cffe8
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-04-13 12:50:30 +1000

    Update main.c

--- laba_06/main.c
+++ laba_06/main.c
@@ -10,24 +10,23 @@ int main()
     char s[10] = "abcdefghij";
     char s2[10] = "0123456789";
     
-    for(int i = 0; i<2000; i++){
+    for(int i = 0; i<20; i++){
         fprintf(fp1, "%c%c %c%c %c%c 19%c%c\n", s[rand()%10],s[rand()%10],s[rand()%10],s[rand()%10],s[rand()%10],s[rand()%10],s2[rand()%10],s2[rand()%10]);
     }
     */
-    printf(" %d",atoi("12"));
+    
     fp1 = fopen("input.txt", "r");
     fp2 = fopen("output.txt", "w");
-    char *s;
+    char s1[100];
     
-    while (fgets(s, 15, fp1)){
-        char *s2;
-        strncpy(s2, strlen(s)-5, 4);
-        if(atoi(s2) > 1980){
-            fprintf(fp2, "%*s", s);
-        }
+    while(fgets(s1, 50, fp1)){
+        char s2[100];
+        strncpy(s2, s1+strlen(s1)-5, strlen(s1));
+        if(atoi(s2) > 1980)
+            fprintf(fp2, "%s", s1);
     }
-    
-    
+    fclose(fp1);
+    fclose(fp2);
 
     return 0;
 }
