43df8f1d140abb7d3cfd20646993806c0f8385fa
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-04-13 13:27:32 +1000

    Update main.c

--- laba_07/main.c
+++ laba_07/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+struct humen{
+    char *name, *sname;
+    int age;
+};
+
+int main(){
+    int N = 4;
+    struct humen a[N], b[N];
+    char name[100], sname[100];
+    int age;
+    for(int i = 0; i<N; i++){
+        printf("введите имя %d-го человека: ", i+1);
+        scanf("%s", name);
+        printf("введите фамилию %d-го человека: ", i+1);
+        scanf("%s", sname);
+        printf("введите возраст %d-го человека: ", i+1);
+        scanf("%d", &age);
+        a[i].name = name;
+        a[i].sname = sname;
+        a[i].age = age;
+    }
+    return 0;
+}
