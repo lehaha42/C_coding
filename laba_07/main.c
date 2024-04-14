f8cc8e34aa1995eb39fe477929f30a2224d4d885
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-04-14 19:40:50 +1000

    Update main.c

--- laba_07/main.c
+++ laba_07/main.c
@@ -1,25 +1,36 @@
+
 #include <stdio.h>
+#include <string.h>
 
 struct humen{
-    char *name, *sname;
-    int age;
+    char name[100], sname[100], pol[10];
+    int byear, height;
 };
 
 int main(){
     int N = 4;
     struct humen a[N], b[N];
-    char name[100], sname[100];
-    int age;
+    char name[100], sname[100], pol[10];
+    int byear, height;
     for(int i = 0; i<N; i++){
         printf("введите имя %d-го человека: ", i+1);
         scanf("%s", name);
         printf("введите фамилию %d-го человека: ", i+1);
         scanf("%s", sname);
-        printf("введите возраст %d-го человека: ", i+1);
-        scanf("%d", &age);
-        a[i].name = name;
-        a[i].sname = sname;
-        a[i].age = age;
+        printf("введите год рождения %d-го человека: ", i+1);
+        scanf("%d", &byear);
+        printf("введите пол %d-го человека: ", i+1);
+        scanf("%s", pol);
+        printf("введите рост %d-го человека: ", i+1);
+        scanf("%d", &height);
+        strcpy(a[i].name, name);
+        strcpy(a[i].sname, sname);
+        strcpy(a[i].pol, pol);
+        a[i].byear = byear;
+        a[i].height = height;
+    }
+    for(int i = 0; i<N; i++){
+        printf("%d %s %s %s %d %d\n", i, a[i].name, a[i].sname, a[i].pol, a[i].byear, a[i].height);
     }
     return 0;
 }
