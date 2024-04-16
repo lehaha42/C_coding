bdb167c0947c35baa3f50890a742820c455fb7af
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-04-16 14:28:55 +1000

    Create main.c

---
+++ laba_11/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "func.h"
+
+
+int main()
+{
+    printf("%d\n", sum(3, 1,2,3));
+    printf("%d\n", max(3, 1,2,3));
+    printf("%d\n", min(3, 1,2,3));
+    printf("%d\n", avg(3, 1,2,3));
+    return 0;
+}
