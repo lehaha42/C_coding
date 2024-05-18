6c19ee9d30315f37fa54aa3ecea86dc35fdcd4ed
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-05-18 13:03:41 +1000

    Update main.c

--- laba_10/main.c
+++ laba_10/main.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+
+
+#define MAX(n, arr) ({int max = (arr)[0]; for (int i = 1; i < n; i++) if (max < (arr)[i]) max = (arr)[i]; max;})
+#define MID(n, arr) ({int sum = (arr)[0]; for (int i = 1; i < n; i++) sum += (arr)[i]; (double)sum / (double)n;})
+
+int main()
+{   
+    int arr[] = {7, 55555, 44444444};
+    printf("%lf", MID(3, arr));
+
+    return 0;
+}
