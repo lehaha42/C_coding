ef5ece93d6528a35dc949ea55f36ae6f2e6f2e33
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-05-19 21:47:45 +1000

    Update main2.c

--- laba_09/main2.c
+++ laba_09/main2.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+float calc(char *s){
+    double ans = 0.0;
+    char s_cpy[100], *p;
+    char *nums[100];
+    strcpy(s_cpy, s);
+    
+    return ans;
+}
+
+
+int main(){
+    float ans = 0.0;
+    char inp[100];
+    scanf("%s", inp);
+    printf("%lf", calc(inp));
+    return 0;
+}
