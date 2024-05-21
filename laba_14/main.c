2808383dbcee4fe9b6a3a36898fd48cfd0532bd4
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-05-21 21:14:21 +1000

    Update main.c

--- laba_14/main.c
+++ laba_14/main.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+#include <setjmp.h>
+
+
+int main(){
+    int a = 1, b = 1, c = 0, n = 0;
+    scanf("%d", &n);
+    jmp_buf env = {0};
+    int val = setjmp(env);
+    c = a + b;
+    a = b;
+    b = c;
+    if(val < n - 3) longjmp(env, val + 1);
+    printf("%d", c);
+    return 0;
+}
