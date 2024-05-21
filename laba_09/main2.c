6e93efb1ba6d0b7c5ea7723a4393589ad043b14c
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-05-21 19:18:33 +1000

    Update main2.c

--- laba_09/main2.c
+++ laba_09/main2.c
@@ -4,11 +4,33 @@
 #include <string.h>
 
 float calc(char *s){
-    double ans = 0.0;
-    char s_cpy[100], *p;
-    char *nums[100];
+    char s_cpy[100], s_cpy2[100];
     strcpy(s_cpy, s);
-    
+    strcpy(s_cpy2, s);
+    char signs[100], nums[100][100];
+    char *p;
+    p = strtok(s_cpy, "0123456789. ");
+    int i = 0;
+    while(p != NULL){
+        if (p[0] == '-') signs[i] = '-';
+        else             signs[i] = '+';
+        i++;
+        p = strtok(NULL, "0123456789. ");
+    }
+    int Nsigns = i; i = 0;
+    p = strtok(s_cpy2, "+- ");
+    while(p != NULL){
+        strcpy(nums[i], p);
+        i++;
+        p = strtok(NULL, "+- ");
+    }
+    int Nnums = i;
+    double ans = atof(nums[0]);
+    for (i = 1; i < Nnums; i++){
+        double n = atof(nums[i]);
+        if (signs[i-1] == '-') ans -= n;
+        else                 ans += n;
+    }
     return ans;
 }
 
@@ -16,7 +38,7 @@ float calc(char *s){
 int main(){
     float ans = 0.0;
     char inp[100];
-    scanf("%s", inp);
+    gets(inp);
     printf("%lf", calc(inp));
     return 0;
 }
