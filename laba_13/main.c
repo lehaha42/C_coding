ade903d47e8e30c16cdf0542f31256ce95a9aeaf
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2024-05-23 20:28:52 +1000

    Update main.c

--- laba_13/main.c
+++ laba_13/main.c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+#include <signal.h>
+
+
+
+
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    raise(n);
+    return 0;
+}
+
+//SIGABRT, SIGFPE, SIGILL, SIGINT, SIGSEGV, SIGTERM
+//6        8       4       2       11       15
